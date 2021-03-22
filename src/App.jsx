68b8cec9a4939cccaa1aa129fd5e607e6a6042c8
Ihor Savelyev <savelyev7979@gmail.com>
2021-03-22 14:44:15 +0200

    geolocation position learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import faker from "faker";
 
 import Comment from "./components/Comment";
 import AppruvalCard from "./components/AppruvalCard";
+import SeasonDisplay from "./components/SeasonDisplay";
 
-const App = () => {
-  return (
-    <div className="ui container comments">
-      <AppruvalCard>
-        <Comment
-          avatar={faker.image.avatar}
-          author="Sam"
-          timeAgo="Today at 6:00PM"
-          commentText="Nice blog post!"
-        />
-      </AppruvalCard>
+export default class App extends Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
 
-      <AppruvalCard>
-        <Comment
-          avatar={faker.image.avatar}
-          author="John"
-          timeAgo="Today at 4:00PM"
-          commentText="Good job!"
-        />
-      </AppruvalCard>
+    return (
+      <div className="ui container comments">
+        <SeasonDisplay />
 
-      <AppruvalCard>
-        <Comment
-          avatar={faker.image.avatar}
-          author="Jane"
-          timeAgo="Yerstarday at 2:00AM"
-          commentText="Very informative!"
-        />
-      </AppruvalCard>
-    </div>
-  );
-};
+        <AppruvalCard>
+          <Comment
+            avatar={faker.image.avatar}
+            author="Sam"
+            timeAgo="Today at 6:00PM"
+            commentText="Nice blog post!"
+          />
+        </AppruvalCard>
 
-export default App;
+        <AppruvalCard>
+          <Comment
+            avatar={faker.image.avatar}
+            author="John"
+            timeAgo="Today at 4:00PM"
+            commentText="Good job!"
+          />
+        </AppruvalCard>
+
+        <AppruvalCard>
+          <Comment
+            avatar={faker.image.avatar}
+            author="Jane"
+            timeAgo="Yerstarday at 2:00AM"
+            commentText="Very informative!"
+          />
+        </AppruvalCard>
+      </div>
+    );
+  }
+}
